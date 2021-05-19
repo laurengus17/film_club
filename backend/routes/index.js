762f68e976af5369c0b79dee293ba1999a8a04f5
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-19 15:40:38 -0700

    backend skeleton done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,13 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
+// router.get('/hello/world', function(req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
 });
 
 module.exports = router;
