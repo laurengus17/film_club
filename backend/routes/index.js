f43aca149a7ab662cef7875fe15b8bac75ccad52
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-19 16:17:45 -0700

    error

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,33 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+const path = require('path');
+// Serve the frontend's index.html file at the root route
+router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+    path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+});
+
+router.use(express.static(path.resolve("../frontend/build")));
+
+router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+    path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+});
+}
+
 // router.get('/hello/world', function(req, res) {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
 //     res.send('Hello World!');
