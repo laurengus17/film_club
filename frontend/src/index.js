036d56a10c72b8be69c26c4d02dd5a34b79d9a2a
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-19 16:17:33 -0700

    error before phase 1 frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
