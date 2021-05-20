d1bb9721e84e512170ef56aa2bbf46ddf20d4a70
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-20 10:07:13 -0700

    login page css

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
+
+function LoginFormPage() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return (
+        <Redirect to="/" />
+    );
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+
+    return dispatch(sessionActions.login({ credential, password }))
+    .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+    });
+}
+
+return (
+<div className='form-outer'>
+    <div className='login-structure'>
+        <form onSubmit={handleSubmit}>
+        <ul>
+            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <h2 className='login-title'>Login</h2>
+        <div className='login-body'>
+                <input
+                className='input'
+                placeholder='Username or Email'
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+                />
+                <input
+                className='input'
+                type="password"
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+        </div>
+        <button type="submit" className='login-button'>Log In</button>
+        </form>
+    </div>
+</div>
+);
+}
+
+export default LoginFormPage;
