e6f89bbfd654f8a22ecb330da51b42d1382d6cf8
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-27 14:23:32 -0700

    home page working

--- frontend/src/components/HomePage/HomePageLanding.js
+++ frontend/src/components/HomePage/HomePageLanding.js
@@ -10,7 +10,10 @@ function HomePageLanding() {
 const dispatch = useDispatch();
 const userState = useSelector(state => state.session);
 const users = Object.values(userState);
-console.log(users, 'THIS IS USERSSS')
+const photoState = useSelector(state => state.photos);
+const photos = Object.values(photoState);
+const albumState = useSelector(state => state.albums);
+const albums = Object.values(albumState);
 
 useEffect(() => {
 dispatch(getAlbums())
@@ -20,17 +23,21 @@ dispatch(getUsers())
 
 return (
     <div>
-    <h2>HI YOU ARE HOME</h2>
+    <h2>Welcome to the Club</h2>
+    <h3>Meet Our Users</h3>
     <div>
         {users.map((user) => {
         if(user) {
             return (
                 <div>
+                    <li key={user.id}>
                     <h2>{user.username}</h2>
-                    <img src={user.profileImageUrl} alt='profile' />
+                    <img src={user.profileImageUrl} alt='profile' className='profile-image' />
+                    </li>
                 </div>
             )
         }
+        return []
         })}
     </div>
     </div>
