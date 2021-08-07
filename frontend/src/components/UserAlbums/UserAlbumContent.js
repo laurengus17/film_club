d65265658d1ca3ead4a0810ad0bc7db7f355db5e
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-08-06 17:00:59 -0700

    updated css for albums page

--- frontend/src/components/UserAlbums/UserAlbumContent.js
+++ frontend/src/components/UserAlbums/UserAlbumContent.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { getAlbums } from '../../store/album';
 import { getPhotos } from '../../store/photo';
+import AddPhotoModal from '../PhotoForm/AddPhotoModal';
 import './UserAlbums.css';
 
 function UserAlbumContent() {
@@ -11,6 +12,8 @@ function UserAlbumContent() {
     const { albumId } = useParams()
     const photoState = useSelector(state => state.photos);
     const photos = Object.values(photoState);
+    const albumState = useSelector(state => state.albums);
+    const albums = Object.values(albumState);
 
     useEffect(() => {
     dispatch(getAlbums())
@@ -19,40 +22,36 @@ function UserAlbumContent() {
 
     const albumIdInteger = parseInt(albumId, 10)
 
+    const currentAlbum = albums.filter((album) => album.id === albumIdInteger)
+    console.log(currentAlbum)
     const myAlbum = () => {
         const albumPhotos = photos.filter((photo) => photo.albumId === albumIdInteger)
             if(albumPhotos) {
                 return (
-                    <div>
-                    <li key={albumId} className='photos-list'>
+                    <>
                         {albumPhotos?.map((photo) => {
                             return (
                                 <div className='image-div'>
-                                <img src={photo?.url} alt='user-posts' className='album-content'/>
-                                <button className='specific-photo-button' onClick={() => history.push(`/display/${photo?.id}`)}>{photo?.title}</button>
+                                    <img src={photo?.url} alt='user-posts' className='album-content'/>
+                                    <button className='specific-photo-button' onClick={() => history.push(`/display/${photo?.id}`)}>{photo?.title}</button>
                                 </div>
                             )
                         })}
-                    </li>
-                    </div>
+                    </>
                 )
             }
     }
 
-    const handleRoute = () => {
-        history.push(`/api/photo/create/${albumId}`)
-    }
-
     return (
-        <div className='album-outer-div'>
-            <h2 className='album-page'>My Album</h2>
-            <div className='add-photo'>
-                <button onClick={handleRoute} className='add-photo-button'>Add Photo</button>
-            </div>
-            <div className='specific-photo-div'>
-                {myAlbum()}
-            </div>
+        <>
+        <h2 className='album-page'>My Album</h2>
+        <div className='add-photo'>
+                <AddPhotoModal albumId={albumId} />
         </div>
+        <div className='album-outer-div'>
+            {myAlbum()}
+        </div>
+        </>
     )
 }
 
