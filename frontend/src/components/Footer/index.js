b793d58f237ee7cdcb88e5b1bcc0beb285df4394
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-08-09 12:07:02 -0700

    setting up footer

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(){
+
+return (
+<div className='outer-footer'>
+    <div className='footer-container'>
+        <ul className='footer-list'>
+            
+        </ul>
+    </div>
+</div>
+);
+}
+
+export default Footer;
